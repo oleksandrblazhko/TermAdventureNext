684ce020b0e1b1049116c58d8dd26fc1bff10536
Oleksandr Blazhko <72593112+oleksandrblazhko@users.noreply.github.com>
2026-04-03 18:53:51 +0300

    Fix CHALLENGE_FILE quoting and cleanup ta_level_start_time
    Fix quoting for CHALLENGE_FILE variable and remove ta_level_start_time.

--- challenger.sh
+++ challenger.sh
@@ -10,12 +10,13 @@ if [ -n "$CHALLENGE_FILE" ]; then
     esac
 fi
 
-if [ ! -e $CHALLENGE_FILE ];
+if [ ! -e "$CHALLENGE_FILE" ];
 then
     echo "Challenge file $CHALLENGE_FILE does not exist."
     exit 1;
 fi
 CHALLENGE_NAME="${CHALLENGE_NAME:-$(basename "$CHALLENGE_FILE" | sed 's/\.[^.]*$//')}"
+
 # Prepare global ENV variables for child processes
 export TA_BIN
 export CHALLENGE_FILE
@@ -26,6 +27,7 @@ bash --rcfile $CURRENT_DIR/ta_bashrc
 # Clean up used files
 rm -rf $HOME/.tahistory
 rm -rf $HOME/.config/$CHALLENGE_NAME
+rm -f $HOME/.ta_level_start_time
 
 # Unset global ENV variables
 unset TA_BIN
